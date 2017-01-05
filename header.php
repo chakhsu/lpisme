74cfaa27a5c14115964dcbed76caeb2a2caf6674
Chakhsu Lau <zexu.liu@live.cn>
2017-01-05 22:08:48 +0800

    update
    add dns-prefetch support

--- header.php
+++ header.php
@@ -2,29 +2,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="<?php $this->options->charset(); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-	<meta name="renderer" content="webkit">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<meta http-equiv="Cache-Control" content="no-transform"/>
-	<meta http-equiv="Cache-Control" content="no-siteapp"/>
-	<title><?php $this->archiveTitle(array(
-	'category'  =>  _t(' %s '),
-	'search'    =>  _t(' %s '),
-	'tag'       =>  _t(' %s '),
-	'author'    =>  _t(' %s ')
-	), '', ' - '); ?><?php $this->options->title(); ?></title>
-	<meta name="keywords" content="<?php $this->keywords() ?>" />
-	<?php $this->header('keywords=&generator=&template=&pingback=&xmlrpc=&wlw=&commentReply=&rss1=&rss2=&atom='); ?>
-	<link rel="shortcut icon" href="<?php if($this->options->favicon): $this->options->favicon(); else: $this->options->themeUrl('images/favicon.png');endif; ?>">
-	<link rel="apple-touch-icon" href="<?php if($this->options->iosicon): $this->options->iosicon(); else: $this->options->themeUrl('images/favicon.png');endif; ?>">
-	<link rel="stylesheet" href="<?php $this->options->themeUrl('assets/prism.css'); ?>">
-	<link rel="stylesheet" href="<?php $this->options->themeUrl('assets/style.css'); ?>">
-	<link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
-	<!--[if lt IE 9]>
-	<script src="//cdn.bootcss.com/html5shiv/r29/html5.min.js"></script>
-	<script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
-	<![endif]-->
+<meta charset="<?php $this->options->charset(); ?>">
+<?php if ($this->options->cdn_add): ?>
+<link rel="dns-prefetch" href="<?php $this->options->cdn_add(); ?>" />
+<link rel="dns-prefetch" href="//cdn.bootcss.com" />
+<?php endif; ?>
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+<meta name="renderer" content="webkit">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta http-equiv="Cache-Control" content="no-transform"/>
+<meta http-equiv="Cache-Control" content="no-siteapp"/>
+<title><?php $this->archiveTitle(array(
+'category'  =>  _t(' %s '),
+'search'    =>  _t(' %s '),
+'tag'       =>  _t(' %s '),
+'author'    =>  _t(' %s ')
+), '', ' - '); ?><?php $this->options->title(); ?></title>
+<meta name="keywords" content="<?php $this->keywords() ?>" />
+<?php $this->header('keywords=&generator=&template=&pingback=&xmlrpc=&wlw=&commentReply=&rss1=&rss2=&atom='); ?>
+<link rel="shortcut icon" href="<?php if($this->options->favicon): $this->options->favicon(); else: $this->options->themeUrl('images/favicon.png');endif; ?>">
+<link rel="apple-touch-icon" href="<?php if($this->options->iosicon): $this->options->iosicon(); else: $this->options->themeUrl('images/favicon.png');endif; ?>">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('assets/prism.css'); ?>">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('assets/style.css'); ?>">
+<link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
+<!--[if lt IE 9]>
+<script src="//cdn.bootcss.com/html5shiv/r29/html5.min.js"></script>
+<script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
 </head>
     <body class="null" gtools_scp_screen_capture_injected="true">
         <!--[if lt IE 9]>
